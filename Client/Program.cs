75ccb5f6b0a2f01df0f0d1ca0d0309abbecfd1b0
quliy_nx74 <quliy_nx74@STHQ0118-06>
2024-09-27 08:35:39 +0400

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.Sockets;
+using System.Net;
+
+namespace Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var client = new TcpClient();
+
+            var port = 27001;
+            var ip = IPAddress.Parse("10.1.18.1");
+            var ep = new IPEndPoint(ip, port);
+
+            Console.ReadKey();
+            try
+            {
+                client.Connect(ep);
+                if (client.Connected)
+                {
+                    while (true)
+                    {
+                        var text = Console.ReadLine();
+                        var sw = new StreamWriter(client.GetStream());
+                        sw.WriteLine(text);
+                        sw.AutoFlush = true;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var port = 27001;
+            var ip = IPAddress.Parse("10.1.18.1");
+            var ep = new IPEndPoint(ip, port);
+
+            using var listener = new TcpListener(ep);
+            listener.Start();
+            Console.WriteLine($"Listening on: {listener.LocalEndpoint}");
+
+            while (true)
+            {
+                var client = listener.AcceptTcpClient();
+                var task = Task.Run(() =>
+                {
+                    Console.WriteLine($"{client.Client.RemoteEndPoint} connected");
+                    var stream = client.GetStream();
+                    var sr = new StreamReader(stream);
+                    while (true)
+                    {
+                        var message = sr.ReadLine();
+                        Console.WriteLine($"{client.Client.RemoteEndPoint}: {message}");
+                    }
+                });
+            }
+        }
+    }
+}
